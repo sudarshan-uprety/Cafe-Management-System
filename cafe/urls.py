7a0a68c06ef51009d9ba812c0ebc97ddfc83aab9
Sudarshan uprety <mr.sudarshanuprety@gmail.com>
2023-05-29 19:50:08 +0545

    added swagger documentation

--- cafe/urls.py
+++ cafe/urls.py
@@ -21,7 +21,26 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Sky & Cloud Cafe API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('user/',include('users.urls')),
